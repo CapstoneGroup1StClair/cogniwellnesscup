ee9c3d6dcc1077364d76fb790096e3b0bf5408bf
Chinmay Chamoli <128532140+CapstoneGroup1StClair@users.noreply.github.com>
2023-09-01 19:19:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,20 +32,19 @@ def get_form_submission():
     data = request.get_json()
     admin_emails = [user['email'] for user in db.Users.find()]
     columns_to_process = ['em1', 'em2', 'em3', 'em4', 'em5', 'em6', 'em7', 'em8', 'em9', 'em10','ph1', 'ph2', 'ph3', 'ph4', 'ph5', 'ph6', 'ph7', 'ph8', 'ph9', 'ph10','sp1', 'sp2', 'sp3', 'sp4', 'sp5', 'sp6', 'sp7', 'sp8', 'sp9', 'sp10','so1', 'so2', 'so3', 'so4', 'so5', 'so6', 'so7', 'so8', 'so9', 'so10','fi1', 'fi2', 'fi3', 'fi4', 'fi5', 'fi6', 'fi7', 'fi8', 'fi9', 'fi10','oc1', 'oc2', 'oc3', 'oc4', 'oc5', 'oc6', 'oc7', 'oc8', 'oc9', 'oc10','in1', 'in2', 'in3', 'in4', 'in5', 'in6', 'in7', 'in8', 'in9', 'in10','en1', 'en2', 'en3', 'en4', 'en5', 'en6', 'en7', 'en8', 'en9', 'en10']
-    for item in data:
-        for col in columns_to_process:
-            if col in item:
-                item[col] = float(item[col]) if item[col] != '' else 4.0
-        emotional_sum = sum(item.get(f'em{i}', 4.0) for i in range(1, 11))
-        occupational_sum = sum(item.get(f'oc{i}', 4.0) for i in range(1, 11))
-        spiritual_sum = sum(item.get(f'sp{i}', 4.0) for i in range(1, 11))
-        physical_sum = sum(item.get(f'ph{i}', 4.0) for i in range(1, 11))
-        social_sum = sum(item.get(f'so{i}', 4.0) for i in range(1, 11))
-        financial_sum = sum(item.get(f'fi{i}', 4.0) for i in range(1, 11))
-        intellectual_sum = sum(item.get(f'in{i}', 4.0) for i in range(1, 11))
-        environmental_sum = sum(item.get(f'en{i}', 4.0) for i in range(1, 11))
-        dimensions = {'Emotional': emotional_sum,'Occupational': occupational_sum,'Spiritual': spiritual_sum,'Physical': physical_sum,'Social': social_sum,'Financial': financial_sum,'Intellectual': intellectual_sum,'Environmental': environmental_sum}
-        item['dimension'] = min(dimensions, key=dimensions.get)
+    for col in columns_to_process:
+        if col in item:
+            item[col] = float(item[col]) if item[col] != '' else 4.0
+    emotional_sum = sum(data.get(f'em{i}', 4.0) for i in range(1, 11))
+    occupational_sum = sum(data.get(f'oc{i}', 4.0) for i in range(1, 11))
+    spiritual_sum = sum(data.get(f'sp{i}', 4.0) for i in range(1, 11))
+    physical_sum = sum(data.get(f'ph{i}', 4.0) for i in range(1, 11))
+    social_sum = sum(data.get(f'so{i}', 4.0) for i in range(1, 11))
+    financial_sum = sum(data.get(f'fi{i}', 4.0) for i in range(1, 11))
+    intellectual_sum = sum(data.get(f'in{i}', 4.0) for i in range(1, 11))
+    environmental_sum = sum(data.get(f'en{i}', 4.0) for i in range(1, 11))
+    dimensions = {'Emotional': emotional_sum,'Occupational': occupational_sum,'Spiritual': spiritual_sum,'Physical': physical_sum,'Social': social_sum,'Financial': financial_sum,'Intellectual': intellectual_sum,'Environmental': environmental_sum}
+    data['dimension'] = min(dimensions, key=dimensions.get)
     db.wellness.insert_one(data)
     msg = Message('Wellness Cup assessment: New Submission Receieved!', recipients=admin_emails)
     msg.body = render_template('cognixrsummary.html', **data)
