b1c58cde017037d402417b1fda5bd420cbd163b1
Chinmay Chamoli <128532140+CapstoneGroup1StClair@users.noreply.github.com>
2023-09-01 20:01:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mail = Mail(app)
 def index():
     if (db.wellness.count_documents({}) > 0):
         sample_record = db.wellness.find_one({}, sort=[( '_id', -1 )])
-        admin_emails = [user['email'] for user in db.Users.find()]
+        admin_emails = [user['email'] for user in db.Users.find_one({}, sort=[( '_id', -1 )])]
         msg = Message('Health and Wellness Survey: New Submission Receieved!', recipients=admin_emails)
         msg.body = render_template('cognixrsummary.html', **sample_record)
         msg.html = render_template('cognixrsummary.html', **sample_record)
@@ -30,7 +30,6 @@ def index():
 @app.post('/')
 def get_form_submission():
     data = request.get_json()
-    admin_emails = [user['email'] for user in db.Users.find()]
     columns_to_process = ['em1', 'em2', 'em3', 'em4', 'em5', 'em6', 'em7', 'em8', 'em9', 'em10','ph1', 'ph2', 'ph3', 'ph4', 'ph5', 'ph6', 'ph7', 'ph8', 'ph9', 'ph10','sp1', 'sp2', 'sp3', 'sp4', 'sp5', 'sp6', 'sp7', 'sp8', 'sp9', 'sp10','so1', 'so2', 'so3', 'so4', 'so5', 'so6', 'so7', 'so8', 'so9', 'so10','fi1', 'fi2', 'fi3', 'fi4', 'fi5', 'fi6', 'fi7', 'fi8', 'fi9', 'fi10','oc1', 'oc2', 'oc3', 'oc4', 'oc5', 'oc6', 'oc7', 'oc8', 'oc9', 'oc10','in1', 'in2', 'in3', 'in4', 'in5', 'in6', 'in7', 'in8', 'in9', 'in10','en1', 'en2', 'en3', 'en4', 'en5', 'en6', 'en7', 'en8', 'en9', 'en10']
     for col in columns_to_process:
         if col in data:
@@ -46,7 +45,8 @@ def get_form_submission():
     dimensions = {'Emotional': emotional_sum,'Occupational': occupational_sum,'Spiritual': spiritual_sum,'Physical': physical_sum,'Social': social_sum,'Financial': financial_sum,'Intellectual': intellectual_sum,'Environmental': environmental_sum}
     data['dimension'] = min(dimensions, key=dimensions.get)
     db.wellness.insert_one(data)
-    msg = Message('Wellness Cup assessment: New Submission Receieved!', recipients=admin_emails)
+    recipient_email = data.get('email')
+    msg = Message('Wellness Cup assessment: New Submission Receieved!', recipients=[recipient_email])
     msg.body = render_template('cognixrsummary.html', **data)
     msg.html = render_template('cognixrsummary.html', **data)
     mail.send(msg)
